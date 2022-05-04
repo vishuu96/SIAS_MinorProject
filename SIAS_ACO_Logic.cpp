d581ffae29d6b3afa8b9cb8bd5ab4214eeb795cd
Aachman Mittal <m.aachman@gmail.com>
2022-05-04 23:00:00 +0530

    Random generators and STDOUT output

--- SIAS_ACO_Logic.cpp
+++ SIAS_ACO_Logic.cpp
@@ -4,9 +4,11 @@
 */
 
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
-enum {
+enum DIRECTIONS{
     DIRECTION_NORTH,
     DIRECTION_EAST,
     DIRECTION_SOUTH,
@@ -21,12 +23,8 @@ struct Packet {
 };
 
 struct Phermone_Levels {
-    float phermone_north;    
-    float phermone_east;
-    float phermone_south;
-    float phermone_west;    
-} PL;
-
+    float phermone_north, phermone_east, phermone_south, phermone_west;    
+} PL = {0.0, 0.0, 0.0, 0.0};
 
 
 float generate_random_phermone(void);   //generate a random value x: 0.0 > x <= 1.0
@@ -36,21 +34,50 @@ Packet generate_random_packet(void);    //generate a random packet,
 
 int main()
 {
+    srand(time(0)); //set seed for all rand() operations
 
-    while(true)
+    int packet_id = 0;  
+    while(packet_id < 100)
     {
         /* 
         TODO: 
-        *   get a random packet generated
+        *   get a random packet generated (done)
         *   update the phermone level for node
         *   make the routing decision for the node (most important logic)  
         *   print output for this in STDOUT
         */
+        packet_id++;
+        Packet packet = generate_random_packet();
+
+        //logic to update phermone levels goes here
+
+        //-----------------------------------------
+
+        //logic to take routing decision goes here
+
+        //----------------------------------------
+
+        cout << packet_id << "," << packet.dir << "," << packet.phermone << "," << PL.phermone_north << "," << PL.phermone_east << "," << PL.phermone_south << "," << PL.phermone_west << "," << packet.Dcoord.first << "," << packet.Dcoord.second << ","/* add decision*/ << endl; //can pipe this output in a csv file from terminal at execution.
     }
 
     return 0;
 }
 
+float generate_random_phermone(void)
+{
+    return 1.0 / (rand() % 10);
+}
+
+Packet generate_random_packet(void)
+{
+    Packet new_packet;
+    new_packet.dir = (rand() % 4);
+    new_packet.Scoord = {rand() % 8, rand() % 8};
+    new_packet.Dcoord = {rand() % 8, rand() % 8};
+    new_packet.phermone = generate_random_phermone();
+
+    return new_packet;
+}
 
 /*
 *   Notes for actual implementation
